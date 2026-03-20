3922e2fce959c7098e17a140dfa023fe51318d01
Michael <liahimkusenko@gmail.com>
2026-03-20 20:21:10 +0300

    .

--- lexer/target/debug/deps/lexer-fd53a58cd669354d.d
+++
@@ -1,6 +0,0 @@
-/home/michael/Projects/school-project-interpreter/lexer/target/debug/deps/lexer-fd53a58cd669354d.d: src/lib.rs src/typeof_token.rs
-
-/home/michael/Projects/school-project-interpreter/lexer/target/debug/deps/liblexer-fd53a58cd669354d.rmeta: src/lib.rs src/typeof_token.rs
-
-src/lib.rs:
-src/typeof_token.rs:
